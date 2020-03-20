0e0e6b24bf16dbdfd50866dcd459e15e65976618
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-20 20:05:46 +0530

    ADDED: FlipCoin for 2 Coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,39 +1,107 @@
 #!/bin/bash -x
 #This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet
+
 echo "-------------------------------------------------------------"
 echo "-------------Welcome To Flip Coin Simulator------------------"
 echo "-------------------------------------------------------------"
-#Declaire Variable
-declare -A coinSideDict
-Hd=0
-Tl=0
 
-#Function For flip
-function flip()
+#Function For Simulator
+function simulator()
 {
-	read -p "Enter How Many Times You want To FlipCoin : " flips
 
-		for((index=0;index<flips;index++))
+#Declaring Variable
+
+key=""
+flips=$1
+coins=$2
+
+#Declairing Dictionary
+
+declare -A coinSideDict
+
+	for((indexOne=1;indexOne<=$flips;indexOne++))
+	do
+
+		for((indexTwo=1;indexTwo<=$coins;indexTwo++))
 		do
-            if [[ $((RANDOM % 2)) -eq 1 ]]
-            then
-                  coinSideDict["result"$index]="Head"
-						((Hd++))
-            else
-                  
-                  coinSideDict["result"$index]="Tail"
-						((Tl++))
-            fi
-		done
 
-echo "Heads Percentage: $(echo "scale=2; $Hd * 100 / $flips" | bc )"
-echo "Tails Percentage: $(echo "scale=2; $Tl * 100 / $flips" | bc )"
+		    if (( $((RANDOM%2==0)) ))
+          then
+
+                  key+=H
+	       else
+
+                  key+=T
+          fi
+
+		done
+		coinSideDict[$key]=$((${coinSideDict[$key]}+1))
+		key=""
+
+	done
+
+	echo  "All Key: "${!coinSideDict[@]}
+	echo  "All Number Of Key: "${coinSideDict[@]}
+
+#Calling The Percentage Function
+
+percentage
+
 }
 
-#Calling The flip Function
-flip
-#Print The HEADS And TAILS 
-echo  "All Occurrence: "${coinSideDict[@]}
+
+
+#Fuction For Calculate The Percentage  
+
+function percentage()
+{
+	for key in ${!coinSideDict[@]}
+	do
+
+		coinSideDict[$key]=$((${coinSideDict[$key]} * 100 / $flips))
+
+	done
+
+	echo  "All Key: "${!coinSideDict[@]}
+	echo  "All Number Of Key Percentage: "${coinSideDict[@]}
+
+}
+
+
+
+#Function For The Start Game
+
+function gameStart()
+{
+	local flips=0
+	read -p "Entered Y To Play Otherwise Key For Quite: " play
+
+	while [[ $((play)) -eq 'Y' ]]
+	do
+
+		read -p "Enter How Many Times You want To FlipCoin : " flips
+		echo -e "Choose The 1 For Singlet Coins: \nChoose The 2 For Doublet Coins: "
+		read choice
+		case $choice in
+				1)
+						simulator $flips 1 ;;
+				2)
+						simulator $flips 2 ;;
+				*)
+						echo "Invalid Option" ;;
+		esac
+
+	read -p "Entered X To Play Otherwise Key For Quite: " play
+	done
+
+}
+
+#Calling The Functions
+
+gameStart
+
+simulator
+
 echo "-------------------------------------------------------------"
 echo "-------------------------------------------------------------"
 echo "-------------------------------------------------------------"
