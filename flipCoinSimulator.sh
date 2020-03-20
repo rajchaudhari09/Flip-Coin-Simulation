aa05a40c3631cfd71cad23bf22572dd799ffe3c9
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-20 23:01:18 +0530

    ADDED: FlipCoin for 3 Coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -25,7 +25,7 @@ declare -A coinSideDict
 		for((indexTwo=1;indexTwo<=$coins;indexTwo++))
 		do
 
-		    if (( $((RANDOM%2==0)) ))
+		    if(( $((RANDOM%2==0)) ))
           then
 
                   key+=H
@@ -40,16 +40,14 @@ declare -A coinSideDict
 
 	done
 
-	echo  "All Key: "${!coinSideDict[@]}
-	echo  "All Number Of Key: "${coinSideDict[@]}
+	echo  "All Key: " ${!coinSideDict[@]}
+	echo  "All Number Of Key: " ${coinSideDict[@]}
 
 #Calling The Percentage Function
 
 percentage
 
 }
-
-
 
 #Fuction For Calculate The Percentage  
 
@@ -67,40 +65,42 @@ function percentage()
 
 }
 
-
-
 #Function For The Start Game
 
-function gameStart()
+function mainFunction()
 {
 	local flips=0
-	read -p "Entered Y To Play Otherwise Key For Quite: " play
+	read -p "Entered Y To Play Otherwise Any Keys For Quite: " play
 
-	while [[ $((play)) -eq 'Y' ]]
+	while [ "$play" == 'y' ]
 	do
 
 		read -p "Enter How Many Times You want To FlipCoin : " flips
-		echo -e "Choose The 1 For Singlet Coins: \nChoose The 2 For Doublet Coins: "
+		echo -e "Choose The 1 For Singlet Coins: \nChoose The 2 For Doublet Coins: \nChoose The 3 For Triplet Coins: "
 		read choice
 		case $choice in
 				1)
-						simulator $flips 1 ;;
+						simulator $flips 1		#Calling The Simulator Function
+						;;
 				2)
-						simulator $flips 2 ;;
+						simulator $flips 2		#Calling The Simulator Function
+						;;
+				3)
+						simulator $flips 3		#Calling The Simulator Function
+						;;
 				*)
-						echo "Invalid Option" ;;
+						echo "Invalid Option"
+						;;
 		esac
+		read -p "Entered y To Play Otherwise Any Keys For Quite: " play
 
-	read -p "Entered X To Play Otherwise Key For Quite: " play
 	done
 
 }
 
-#Calling The Functions
+#Calling The mainFunctions
 
-gameStart
-
-simulator
+mainFunction
 
 echo "-------------------------------------------------------------"
 echo "-------------------------------------------------------------"
